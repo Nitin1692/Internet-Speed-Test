e53a3cd7e4bc5c62e9b0a89b72115d47ebb14618
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-06 19:10:09 +0530

    Added top image

--- speedTest.py
+++ speedTest.py
@@ -5,12 +5,12 @@ root = Tk()
 root.title("Internet Speed Test")
 root.geometry("360x600")
 root.resizable(False,False)
-root.configure(bg="#949494")
+root.configure(bg="#1a212d")
 
 logo = PhotoImage(file="logo.png")
 root.iconphoto(False,logo)
 
 top = PhotoImage(file="top.png")
-Label(root,image=top,bg="#1a212d").pack()
+Label(root,image=top).pack()
 
 root.mainloop()
